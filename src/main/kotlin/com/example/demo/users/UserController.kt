77aea4e9547024953d9b0e766533e787e3701cc0
dodo <dodo45133@gmail.com>
2023-12-31 16:06:33 +0900

    user api에 인증 처리 추가

--- src/main/kotlin/com/example/demo/users/UserController.kt
+++ src/main/kotlin/com/example/demo/users/UserController.kt
@@ -1,11 +1,22 @@
 package com.example.demo.users
 
+import com.example.demo.JwtService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class UserController(private val userService: UserService) {
+class UserController(
+    private val userService: UserService,
+    private val jwtService: JwtService
+) {
 
     @GetMapping("users")
-    fun getUsers(): String = userService.getUsers(1L)
+    fun getUsers(
+        @RequestHeader("Authorization") header: String
+    ): String {
+        val userId = jwtService.verifyJwt(header)
+
+        return userService.getUsers(userId)
+    }
 }
--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.exchange
 import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,9 +21,18 @@ class DemoApplicationTests {
     var randomServerPort: Int = 0
 
     @Test
-    fun `유저정보를 가져온다`() {
-        val users = testRestTemplate.getForObject("http://localhost:$randomServerPort/users", String::class.java)
-        assertEquals("heeve", users)
+    fun `jwt를 이용해서 유저정보를 가져온다`() {
+
+        // #1 로그인을 요청해서 JWT 토큰을 받는다.
+        val jwt = testRestTemplate.getForObject("http://localhost:$randomServerPort/login", String::class.java)
+
+        // #2 JWT 토큰을 이용해서 유저정보를 가져온다.
+        val header = HttpHeaders()
+        header.set("Authorization", "bearer $jwt")
+        val entity = HttpEntity<String>(header)
+        val response = testRestTemplate.exchange<String>("http://localhost:$randomServerPort/users", HttpMethod.GET, entity)
+
+        assertEquals("heeve", response.body)
     }
 
     @Test
